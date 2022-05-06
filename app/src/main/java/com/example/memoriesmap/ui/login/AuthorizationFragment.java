073fae44e7150bfd614741da430b7cda3ff4d679
Soneech <sonya.patrina@yandex.ru>
2022-05-06 18:11:05 +0300

    authorization model class is almost complete

--- app/src/main/java/com/example/memoriesmap/ui/login/AuthorizationFragment.java
+++ app/src/main/java/com/example/memoriesmap/ui/login/AuthorizationFragment.java
@@ -1,39 +1,23 @@
 package com.example.memoriesmap.ui.login;
 
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.example.memoriesmap.databinding.AuthorizationFragmentBinding;
-
-import com.example.memoriesmap.R;
 import com.example.memoriesmap.fragments.FragmentsActions;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class AuthorizationFragment extends Fragment {
 
-    private LoginViewModel loginViewModel;
     private AuthorizationFragmentBinding binding;
     private FragmentsActions fragmentsActions;
-
-    private FirebaseAuth mAuth;
 
     private String email;
     private String password;
@@ -42,7 +26,6 @@ public class AuthorizationFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAuth = FirebaseAuth.getInstance();
     }
 
     @Nullable
@@ -55,7 +38,7 @@ public class AuthorizationFragment extends Fragment {
         binding.authorizationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                createAuthorizationModel();
             }
         });
 
@@ -69,4 +52,22 @@ public class AuthorizationFragment extends Fragment {
         fragmentsActions = (FragmentsActions) context;
     }
 
+    public void createAuthorizationModel() {
+        email = binding.authEmailEditText.getText().toString();
+        password = binding.authPasswordEditText.getText().toString();
+        rememberMe = binding.rememberMeCheckBox.isChecked();
+
+        AuthorizationModel authorizationModel = new AuthorizationModel(
+                email,
+                password,
+                rememberMe,
+                getActivity());
+        if (authorizationModel.signIn()) {
+            updateUI();
+        }
+    }
+
+    public void updateUI() {
+        // something actions...
+    }
 }
--- app/src/main/java/com/example/memoriesmap/ui/login/AuthorizationModel.java
+++ app/src/main/java/com/example/memoriesmap/ui/login/AuthorizationModel.java
@@ -1,5 +1,47 @@
 package com.example.memoriesmap.ui.login;
 
+import android.app.Activity;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.example.memoriesmap.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AuthorizationModel {
 
+    private String email;
+    private String password;
+    private boolean rememberMe;
+    private Activity context;
+
+    private FirebaseAuth mAuth;
+
+    public AuthorizationModel(String email, String password, boolean rememberMe, Activity context) {
+        this.email = email;
+        this.password = password;
+        this.rememberMe = rememberMe;
+        this.context = context;
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public boolean signIn() {
+        final boolean[] isUserExist = {false};
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(context, R.string.welcome, Toast.LENGTH_SHORT).show();
+                    isUserExist[0] = true;
+                }
+                else {
+                    Toast.makeText(context, R.string.user_doesnt_exist, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        return isUserExist[0];
+    }
 }
