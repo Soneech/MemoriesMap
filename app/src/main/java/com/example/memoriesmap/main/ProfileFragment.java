75f9f23f9d937717e48198882ff89e07a4bd5592
Soneech <sonya.patrina@yandex.ru>
2022-05-17 23:59:46 +0300

    profile fragment (currently empty)

---
+++ app/src/main/java/com/example/memoriesmap/main/ProfileFragment.java
@@ -0,0 +1,29 @@
+package com.example.memoriesmap.main;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.memoriesmap.R;
+
+public class ProfileFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.profile_fragment, container, false);
+    }
+
+    public void displayUserData() {
+
+    }
+}
